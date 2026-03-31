1e27190287d0926e4a8522954ef46acb33b3d6b2
root <root@localhost.localdomain>
2026-03-31 18:56:37 +0800

    Simplify project: use static export, remove API routes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -8,32 +9,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    optimizePackageImports: ['react', 'react-dom'],
-  },
-  // Split chunks to avoid 25MB limit
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: 'commons',
-            chunks: 'all',
-            minChunks: 2,
-            maxSize: 20000000, // 20MB max
-          },
-        },
-      };
-    }
-    return config;
   },
 }
 
